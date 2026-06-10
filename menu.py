0ae87ee7e7eaa69f0feefb10c3e631b26100bff8
Siddha Kadam <siddhakad17@gmail.com>
2026-06-10 23:47:17 +0530

    Refactor get_items and improve case-insensitive search

--- menu.py
+++ menu.py
@@ -21,14 +21,18 @@ class Menu:
 
     def get_items(self):
         """Returns all the names of the available menu items"""
-        options = ""
-        for item in self.menu:
-            options += f"{item.name}/"
-        return options
+        # options = ""
+        # for item in self.menu:
+        #     options += f"{item.name}/"
+        # return options
+        
+        # OR
+        
+        return "/".join(item.name for item in self.menu)
 
     def find_drink(self, order_name):
         """Searches the menu for a particular drink by name. Returns that item if it exists, otherwise returns None"""
         for item in self.menu:
-            if item.name == order_name:
+            if item.name.lower() == order_name.lower():
                 return item
         print("Sorry that item is not available.")
